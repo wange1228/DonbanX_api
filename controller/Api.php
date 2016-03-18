23793f0454fa96e5411deb547c45cba2eec10d91
wange <i@wange.im>
2016-03-18 09:36:15 +0800

    preg_match rating_percentage

--- controller/Api.php
+++ controller/Api.php
@@ -13,33 +13,38 @@ class Api {
     /**
      * 抓取详情页
      */
-    private function fetch_douban_detail($url) {
+    private function fetch_douban_detail($url, $type) {
         // $this->load->library("snoopy");
         // 替换http，避免302跳转
         $url = str_replace("http://", "https://", $url);
         $this->snoopy->fetch($url);
         $detail_str = $this->snoopy->results;
 
-        preg_match('/movie\.douban\.com\/subject\/(\d+)/i', $url, $match_id);
+        if ($type === "movie") {
+            preg_match('/movie\.douban\.com\/subject\/(\d+)/i', $url, $match_id);
+        } else if ($type === "book") {
+            preg_match('/book\.douban\.com\/subject\/(\d+)/i', $url, $match_id);
+        }
         preg_match('/<strong .*property="v:average">([0-9]\.[0-9])?<\/strong>/i', $detail_str, $match_average);
         preg_match('/<span property="v:votes">(\d+)?<\/span>/i', $detail_str, $match_vote);
         preg_match('/<div class="ll bigstar(\d{2})"><\/div>/i', $detail_str, $match_star);
         preg_match('/<span property="v:itemreviewed">(.*)?<\/span>/i', $detail_str, $match_name);
+        preg_match_all('/<span class="rating_per">([0-9]+\.[0-9])?\%<\/span>/i', $detail_str, $match_rate);
 
         $id = $match_id[1];
         $name = isset($match_name[1]) ? $match_name[1] : "";
         $average = isset($match_average[1]) ? $match_average[1] : "0.0";
         $vote = isset($match_vote[1]) ? $match_vote[1] : 0;
         $star = isset($match_star[1]) ? $match_star[1] : "00";
-
-        // log_message("info", "DONE: $id\t$name");
+        $rate = isset($match_rate[1]) ? $match_rate[1] : array();
 
         return array(
             "id" => $id,
             "name" => $name,
             "average" => $average,
             "vote" => $vote,
-            "star" => $star
+            "star" => $star,
+            "rate" => $rate
         );
     }
 
@@ -49,7 +54,7 @@ class Api {
     public function get_rate() {
         $name = trim(htmlspecialchars($_POST["name"]));
         $type = trim(htmlspecialchars($_POST["type"]));
-        if ($name !== "") {
+        if ($name !== "" && in_array($type, array("movie", "book"))) {
             $rate = $this->get_rate_offline($name, $type);
             $rate = (isset($rate) && !empty($rate)) ? $rate : $this->get_rate_online($name, $type);
         } else {
@@ -87,13 +92,17 @@ class Api {
         $url = "https://$type.douban.com/subject_search?search_text=$name";
         $this->snoopy->fetch($url);
         $search_str = $this->snoopy->results;
-        preg_match('/<a class="nbg" href="https:\/\/movie\.douban\.com\/subject\/(\d+)?\/" onclick/i', $search_str, $match_id);
+        if ($type === "movie") {
+            preg_match('/<a class="nbg" href="https:\/\/movie\.douban\.com\/subject\/(\d+)?\/" onclick/i', $search_str, $match_id);
+        } else if ($type === "book") {
+            preg_match('/<a class="nbg" href="https:\/\/book\.douban\.com\/subject\/(\d+)?\/" onclick/i', $search_str, $match_id);
+        }
 
         $output = NULL;
         if (isset($match_id[1])) {
             $id = $match_id[1];
-            $url = "https://movie.douban.com/subject/$id/";
-            $rate = $this->fetch_douban_detail($url);
+            $url = "https://$type.douban.com/subject/$id/";
+            $rate = $this->fetch_douban_detail($url, $type);
 
             $average = $rate["average"];
             $vote = $rate["vote"];
